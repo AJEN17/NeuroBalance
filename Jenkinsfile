a68411a3b28e1304a181bff6c0eb31278c81a4eb
Ajendra <ajendra@Ajendras-MacBook-Air.local>
2026-04-14 22:28:47 +0530

    Clean initial commit

---
+++ Jenkinsfile
@@ -0,0 +1,36 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Clone Repository') {
+            steps {
+                git branch: 'main', url: 'https://github.com/YOUR_USERNAME/NeuroBalance.git'
+            }
+        }
+        
+        stage('Tear Down Old Environment') {
+            steps {
+                sh 'docker compose down || true'
+                sh 'docker network prune -f || true'
+            }
+        }
+
+        stage('Build Docker Images') {
+            steps {
+                sh 'docker compose build'
+            }
+        }
+
+        stage('Deploy Full Stack') {
+            steps {
+                sh 'docker compose up -d'
+            }
+        }
+        
+        stage('Verify Deployment') {
+            steps {
+                sh 'docker ps'
+            }
+        }
+    }
+}
---
+++ infra/main.tf
@@ -0,0 +1,90 @@
+provider "aws" {
+  region = "us-east-1" # Change to your region if you are not in us-east-1 (N. Virginia)
+}
+
+# Dynamically grab the latest Ubuntu 22.04 AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+resource "aws_security_group" "neuro_sg" {
+  name        = "neurobalance-sg"
+  description = "Allow all necessary ports for the DevOps stack"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Frontend"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Backend API"
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Jenkins"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Prometheus"
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Grafana"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "neuro_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro" 
+  
+  key_name      = "jenkins-key" 
+  
+  vpc_security_group_ids = [aws_security_group.neuro_sg.id]
+
+  tags = {
+    Name = "NeuroBalance-DevOps-Server"
+  }
+}
+
+output "server_public_ip" {
+  value = aws_instance.neuro_server.public_ip
+}
